882bb9757f46839a92a0ee2a5f29cf40826098e1
Zhan Ilyas <zhan@init.kz>
2021-01-12 12:23:36 +0600

    ref SendSms::sendSms

--- app/SendSms/SendSms.php
+++ app/SendSms/SendSms.php
@@ -45,14 +45,20 @@ class SendSms
         return $token;
     }
 
-    protected static function sendSms($phone_number, $message)
+    protected static function sendSms($phone_numbers, $message)
     {
         $link = 'https://smsc.kz/sys/send.php';
+
+        if (is_array($phone_numbers)) {
+            $phone_numbers = implode(';', $phone_numbers);
+        }
+
+        return $phone_numbers;
 
         Http::get($link, [
             'login' => 'mykidkz',
             'psw' => 'f39391baadf771e31384e90bc3e1603796733356',
-            'phones' => $phone_number,
+            'phones' => $phone_numbers,
             'mes' => $message,
         ]);
     }
