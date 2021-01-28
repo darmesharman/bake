a0dba9fa5649aa527c9385efc6bcb04359cda826
Arman Darmesh <darmesharman@gmail.com>
2021-01-28 16:53:05 +0600

    make Seeder for Blog

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BlogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Blog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph(20),
+            'user_id' => User::factory(),
+        ];
+    }
+}
